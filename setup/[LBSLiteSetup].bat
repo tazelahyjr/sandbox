ffdf124c6b786f7fef93666ac5ef11c6fc173ce3
Tom Zelahy <tomz@eMoneyAdvisor.com>
2016-08-19 12:30:37 -0400

    add setup folder

---
+++ setup/[LBSLiteSetup].bat
@@ -0,0 +1,4 @@
+CD\
+CD C:\projects.net\guardian\LBSLite
+Setup\bin\Debug\liteSetup.exe virtual=lite database=lite serviceusername=TOMZ01\emaserviceuser servicepassword=Password1 webfolder=C:\projects.net\guardian\LBSLite\lite idpurl=https://dev.lbs.emoneyadvisor.com/lbs.corped redirecturl=https://dev.lbs.emoneyadvisor.com clientid=lbslite clientsecret=Pa55word => c:\logLBSLite.txt
+pause
