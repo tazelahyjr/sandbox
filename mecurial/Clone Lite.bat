832d69f14e7a26cacd7ddabb3284a89321e3fb83
Tom Zelahy <tomz@eMoneyAdvisor.com>
2016-08-19 13:44:58 -0400

    adding old mecurial files

---
+++ mecurial/Clone Lite.bat
@@ -0,0 +1,2 @@
+cd\
+hg clone http://emalbslab01/hg/Guardian/LBSLite/ C:\projects.net\guardian\LBSLite
---
+++ mecurial/Clone.bat
@@ -0,0 +1,2 @@
+cd\
+hg clone http://emalbslab01/hg/Guardian/source C:\projects.net\guardian\Chat
